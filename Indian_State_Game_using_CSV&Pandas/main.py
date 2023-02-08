1d881db4eddea6d9588d70c163c6a3535a806079
Ratna Kadam <123215740+RatnaKadam@users.noreply.github.com>
2023-02-08 18:25:15 +0100

    Added remaining state name list
    Added the list of state names which are not guessed and converted that list into a CSV file

--- Indian_State_Game_using_CSV&Pandas/main.py
+++ Indian_State_Game_using_CSV&Pandas/main.py
@@ -11,9 +11,13 @@ score = 0
 data = pandas.read_csv("Indian_states.csv")
 state_names = data.state.to_list()
 
+# List of guessed names
+guessed_list = []
+
 # check if user's answer is in the state list
 while score < 38:
     if user_name in state_names:
+        guessed_list.append(user_name)
         tim = turtle.Turtle()
         tim.hideturtle()
         tim.penup()
@@ -22,7 +26,21 @@ while score < 38:
         tim.write(arg=user_name)
         score = score+1
         user_name = my_screen.textinput(title=f"{score}/39", prompt="Enter the state name: ")
+
+    elif user_name == "Exit":
+        break
     else:
         user_name = my_screen.textinput(title=f"{score}/39", prompt="Enter the state name: ")
-
 my_screen.mainloop()
+
+# creating a remaining names list
+remaining_list = []
+for name in state_names:
+    if name not in guessed_list:
+        remaining_list.append(name)
+
+# Creating a Dataframe of remaining list
+df = pandas.DataFrame(remaining_list)
+
+# Converting Dataframe to CSV file
+df.to_csv("Remaining_state_names.csv")
